a9b98164ccc1f2caa07c1be4e8d578ccc7763916
Simon leroux <simonlerou@gmail.com>
2019-12-06 14:58:20 -0500

    Contuation de travayage
    PENIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- StarWar/Assets/player.cs
+++ StarWar/Assets/player.cs
@@ -7,6 +7,7 @@ public class player : MonoBehaviour
 {
     [SerializeField]GameObject _eclair;
     [SerializeField] GameObject _force;
+    [SerializeField] GameObject _epee;
     int layerMask = 10;
     // Start is called before the first frame update
     void Start()
@@ -19,9 +20,18 @@ public class player : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Force();
+           youSpinMeRightRound();
         }
+        
        
+    }
+
+    private void youSpinMeRightRound()
+    {
+        Vector3 Position = new Vector3(Input.mousePosition.x / Screen.width * 32, Input.mousePosition.y / Screen.height * 18);
+        Vector3 vec = Position - transform.position;
+        float rot = (float)(Mathf.Rad2Deg * (Math.Tan(vec.x / vec.y)));
+        StartCoroutine(Spin(rot));
     }
 
     private void Force()
@@ -31,11 +41,27 @@ public class player : MonoBehaviour
         float rot = (float)(Mathf.Rad2Deg * (Math.Tan(vec.x / vec.y)));
         StartCoroutine(fForce(rot));
     }
-
+    IEnumerator Spin(float rot)
+    {
+        GameObject _Repee = Instantiate(_epee, transform.position, Quaternion.identity);
+        _Repee.transform.Rotate(new Vector3(0f, 0f, rot));
+        yield return StartCoroutine(Tour(_Repee));
+    }
+    IEnumerator Tour(GameObject _Repee)
+    {
+        /*
+        while (transform.position!=new Vector3(_epee.transform.position.x, _epee.transform.position.y, 0f))
+        {
+            _Repee.transform.RotateAround(new Vector3(0f, 0f, 0f), transform.position, 359f);
+        }
+        yield return 0;
+        */
+        yield return 0;
+    }
     IEnumerator fForce(float rot)
     {
-        _force.gameObject.transform.Rotate(0f, 0f, rot);
-        Instantiate(_force, transform.,Quaternion.identity);
+        GameObject _Rforce =Instantiate(_force, transform.position,Quaternion.identity);
+        _Rforce.transform.Rotate(new Vector3(0f, 0f, rot));
         yield return new WaitForSeconds(5f);
     }
 
